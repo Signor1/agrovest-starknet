bbef4e39642d32b3d2ee728ebb71a929af49bff6
signor1 <emmanuelomemgboji@gmail.com>
2025-05-26 00:30:40 +0100

    feat: added marketplace page

---
+++ frontend/src/app/(guest)/marketplace/page.tsx
@@ -0,0 +1,20 @@
+import Categories from "@/components/guest/marketplace/Categories";
+import HeroSection from "@/components/guest/marketplace/HeroSection";
+import OurProcess from "@/components/guest/marketplace/OurProcess";
+import Products from "@/components/guest/marketplace/Products";
+import type { NextPage } from "next";
+import React from 'react'
+
+const MarketPlace: NextPage = () => {
+
+  return (
+    <main className="w-full flex flex-col overflow-x-hidden bg-lightgreen/[40%]">
+      <HeroSection />
+      <Categories />
+      <Products />
+      <OurProcess />
+    </main>
+  );
+};
+
+export default MarketPlace;
