404173fe1783c04fc8ea40220964c8bbcc4c2782
signor1 <emmanuelomemgboji@gmail.com>
2025-05-26 03:49:09 +0100

    feat: added portfolio page

---
+++ frontend/src/app/user/portfolio/page.tsx
@@ -0,0 +1,13 @@
+import React from "react"
+import type { NextPage } from "next"
+import UserPortfolio from "@/components/dashboard/Portfolio"
+
+const Portfolio: NextPage = () => {
+  return (
+    <main className="w-full flex flex-col overflow-x-hidden">
+      <UserPortfolio />
+    </main>
+  )
+}
+
+export default Portfolio
