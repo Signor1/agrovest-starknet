68c63937cf2cf17c44cfdfe9ecd41a8cd84dd6d4
signor1 <emmanuelomemgboji@gmail.com>
2025-05-26 03:25:42 +0100

    feat: added governance page

---
+++ frontend/src/app/user/governance/page.tsx
@@ -0,0 +1,13 @@
+import React from "react"
+import type { NextPage } from "next";
+import DAO from "@/components/dashboard/DAO";
+
+
+const Governance: NextPage = () => {
+  return (
+    <main className="w-full flex flex-col overflow-x-hidden">
+      <DAO />
+    </main>
+  )
+}
+export default Governance
