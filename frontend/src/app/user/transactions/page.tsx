0ae297244e9ec39148d9ab68bfb9f8ef57cee781
signor1 <emmanuelomemgboji@gmail.com>
2025-05-26 03:52:01 +0100

    feat: added transactions page

---
+++ frontend/src/app/user/transactions/page.tsx
@@ -0,0 +1,13 @@
+import React from "react"
+import type { NextPage } from "next"
+import MyCarts from "@/components/dashboard/MyCarts"
+
+const Transactions: NextPage = () => {
+  return (
+    <main className="w-full flex flex-col overflow-x-hidden">
+      <MyCarts />
+    </main>
+  )
+}
+
+export default Transactions
---
+++ frontend/src/app/user/transactions/purchased/page.tsx
@@ -0,0 +1,13 @@
+import React from "react"
+import type { NextPage } from "next"
+import PurchasedItems from "@/components/dashboard/PurchasedItems"
+
+const Purchased: NextPage = () => {
+  return (
+    <main className="w-full flex flex-col overflow-x-hidden">
+      <PurchasedItems />
+    </main>
+  )
+}
+
+export default Purchased
