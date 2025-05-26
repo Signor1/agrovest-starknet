d8bedc0265057c4c7950f01f8dd9f3948038dd21
signor1 <emmanuelomemgboji@gmail.com>
2025-05-26 03:36:08 +0100

    feat: added marketplace

---
+++ frontend/src/app/user/marketplace/page.tsx
@@ -0,0 +1,14 @@
+import React from "react"
+import type { NextPage } from "next";
+import MarketPlace from "@/components/dashboard/UserMarketPlace";
+
+
+const UserMarketPlace: NextPage = () => {
+  return (
+    <main className="w-full flex flex-col overflow-x-hidden">
+      <MarketPlace />
+    </main>
+  )
+}
+
+export default UserMarketPlace
